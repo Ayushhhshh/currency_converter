502874ae9c8a2a1525c4a81d4f023a7ef48822b0
Ayush Jain <ayushsmac@Ayushs-MacBook-Air.local>
2023-12-25 18:24:48 +0530

    Building

---
+++ lib/currency_material_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CurrencyMaterialPage extends StatelessWidget{
+  const CurrencyMaterialPage({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center ,
+            children: [ 
+              Text("Ayush")],
+          )
+        )
+      );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:currency_converter/currency_material_app.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -6,14 +7,12 @@ void main(){
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context){
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("Welcome Ayush !!!"))
-      )
-    );
+   return const MaterialApp(
+    home: CurrencyMaterialPage(),
+    debugShowCheckedModeBanner: false,
+   );
   }
 }
