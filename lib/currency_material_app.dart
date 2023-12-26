39b71bbe961d09739eb5cf3c19fe793ad39893bd
Ayush Jain <ayushsmac@Ayushs-MacBook-Air.local>
2023-12-26 19:22:59 +0530

    Building

--- lib/currency_material_app.dart
+++ lib/currency_material_app.dart
@@ -3,18 +3,31 @@ import 'package:flutter/material.dart';
 class CurrencyMaterialPage extends StatelessWidget{
   const CurrencyMaterialPage({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center ,
-            children: [ 
-              Text("Ayush")],
-          )
-        )
-      );
+      backgroundColor: Colors.blueGrey,
+        body:  Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center ,
+              children: [ 
+                Text("Currency Converter",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic,color: Colors.white70
+                )
+                ),
+                TextField( style : TextStyle(color: Colors.black),
+                decoration: InputDecoration(hintText: "Please enter the amount in USD",
+                hintStyle: TextStyle(color: Colors.black),
+                prefixIcon:Icon(Icons.monetization_on_sharp),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(50)))
+                ), 
+                
+                cursorColor: Colors.white,
+              )],
+            ),
+          ),
+        );
   }
-
 }
