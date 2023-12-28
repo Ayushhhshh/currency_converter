a488c5a83f4849ebf7edec5dc5bd821c7e949f65
Ayush Jain <ayushsmac@Ayushs-MacBook-Air.local>
2023-12-28 22:15:11 +0530

    Building

--- lib/currency_material_app.dart
+++ lib/currency_material_app.dart
@@ -15,20 +15,24 @@ class CurrencyMaterialPage extends StatelessWidget{
                 Text("Currency Converter",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic,color: Colors.white70
                 )
                 ),
-                TextField( style : TextStyle(color: Colors.black),
-                decoration: InputDecoration(hintText: "Please enter the amount in USD",
-                hintStyle: TextStyle(color: Colors.black),
-                prefixIcon:Icon(Icons.monetization_on_sharp),
-                prefixIconColor: Colors.black,
-                filled: true,
-                fillColor: Colors.white,
-                focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
-                enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField( style : TextStyle(color: Colors.black),
+                  decoration: InputDecoration(hintText: "Please enter the amount in USD",
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon:Icon(Icons.monetization_on_sharp),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                  ),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  
+                  cursorColor: Colors.white,
+                  ),
                 ),
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                
-                cursorColor: Colors.white,
-              )],
+                ],
             ),
           ),
         );
