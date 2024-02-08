e2fff81075783c284fe66cb31ad60ce5840bf636
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-02-08 22:22:19 +0530

    Building

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
    return const MaterialApp(
+    title: 'Currency Converter',
     home: CurrencyMaterialPage(),
     debugShowCheckedModeBanner: false,
    );
